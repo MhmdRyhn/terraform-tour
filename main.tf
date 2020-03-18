b8a11751f95d7b432ca7dc0137ee53518dde7451
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2020-03-19 00:08:28 +0600

    updated variable calling syntax

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region                      = "${var.region_name}"
-  access_key                  = "${var.access_key}"
-  secret_key                  = "${var.secret_key}"
+  region                      = var.region_name
+  access_key                  = var.access_key
+  secret_key                  = var.secret_key
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
